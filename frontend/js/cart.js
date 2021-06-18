a7eaac49f23b49a6e5f85b09b461227a017a5f99
Laura Mathieu <lauramathieu.git@gmail.com>
2021-06-18 23:39:28 +0200

    Envoi au serveur fonctionnel

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,27 +1,3 @@
-(function () {
-    'use strict'
-  
-    /* Fetch all the forms we want to apply custom Bootstrap validation styles to*/
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    /* Loop over them and prevent submission*/
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-            test()
-          }
-          
-          
-          form.classList.add('was-validated')
-          
-        }, false)
-      })
-      
-  })()
-
 if (document.readyState == "loading") {
     document.addEventListener("DOMContentLoaded", ready)
 } else {
@@ -48,33 +24,7 @@ function ready() {
         addButton.addEventListener("click", addToCartClicked)
     }
 
-    var confirmButtons = document.getElementsByClassName("confirm-btn")
-    for(var i = 0; i < confirmButtons.length; i++) { /*Listening to add to cart button on products page*/
-        var confirm = confirmButtons[i]
-        confirm.addEventListener("click", test)
-    }
-
-
     updateCartTotal() /*So the cart is always updated*/
-}
-
-function myFunc(e){
-    e.preventDefault();
-}
-
-function test() {
-    document.getElementById("submit-btn").addEventListener("click", function(event){
-        event.preventDefault()
-      });
-    var forms = document.getElementsByClassName('needs-validation')
-            console.log(forms[0].className)
-            if(forms[0].className === "container g-3 m-1 mb-4 p-4 bg-light rounded border needs-validation was-validated") {
-                console.log("ok")
-                confirmClicked()
-            } else {
-                console.log("nope")
-                
-            }
 }
 
 function removeCartItem(event) { /*To remove the item when the button 'remove' is clicked*/
@@ -274,21 +224,19 @@ for (var i = 0; i < cartRows.length; i++) {
     var idElement = cartRow.getElementsByClassName("cart-id")[0].textContent
 }
 
-var firstName = document.getElementById("validationCustom01").value
-var lastName = document.getElementById("validationCustom02").value
-var email = document.getElementById("validationCustomUsername").value
-var address = document.getElementById("validationCustom03").value
-var city = document.getElementById("validationCustom04").value
+var firstName = document.getElementById("validationDefault01").value
+var lastName = document.getElementById("validationDefault02").value
+var email = document.getElementById("validationDefaultMail").value
+var address = document.getElementById("validationDefault03").value
+var city = document.getElementById("validationDefault04").value
 
-var objet = {
-    contact: {
+
+var contact = {
         firstName: firstName,
         lastName: lastName,
         address: address,
         city: city,
         email: email
-    },
-    products: []
     };
 
 function confirmClicked() { /*saving in local storage when article is added to cart in products page*/
@@ -296,35 +244,57 @@ function confirmClicked() { /*saving in local storage when article is added to c
     if (document.getElementById("items-in-cart").innerHTML == `Votre panier est vide.`) { /*If item is not yet in local storage*/
         alert("Votre panier est vide !")
     } else { /*if item already in local storage*/
-        location.replace("../frontend/confirmation.html");
         completeOrder()
     }    
     
 }
 
+function myFunc(e){
+    e.preventDefault();
+    confirmClicked()
+}
+
 
 function completeOrder() {
 
-    console.log("hello")
 
 let total = document.getElementsByClassName("cart-total")[0].innerText
 
 let finalOrder = {};
-finalOrder.contact = objet.contact;
+finalOrder.contact = contact;
 finalOrder.products = [];
-
 
 for (var i = 0; i < localStorage.length; i++) { /*get les items depuis le local storage en fonction des keys*/
     let cartItems = JSON.parse((localStorage.getItem(localStorage.key(i))));
     JSON.parse(localStorage.getItem(cartItems));
-    finalOrder.products.push({id: cartItems.id})
+    finalOrder.products.push(cartItems.id)
     
 }
+
 
 console.log(finalOrder)
 
 
 /*post*/
+
+teddiesUrl = "http://localhost:3000/api/teddies/order";
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("POST", teddiesUrl, true);
+
+xhr.onload = function () {
+  
+    if (this.status == 201) {
+      var order = JSON.parse(this.responseText);
+      let successMsg = document.querySelector(".test-test-test");
+      successMsg.textContent = "Merci pour votre achat  " + order.contact.firstName + " " + order.contact.lastName + " d'un montant de " + total + " €. Votre numéro de commande est le suivant: " + order.orderId + "."
+    } else {
+    }
+  };
+
+xhr.setRequestHeader("Content-type", "application/json");
+xhr.send(JSON.stringify(finalOrder));
 
 }
 
